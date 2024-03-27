a35995a7bacd4d014d642c800d34c03d6582dd2b
Codux <codux@codux.com>
2024-03-27 14:42:36 +0200

    add a style file

--- src/_codux/boards/style-guide.board.tsx
+++ src/_codux/boards/style-guide.board.tsx
@@ -1,8 +1,10 @@
+import '../../styles/common/style-guide.css';
 import { createBoard } from '@wixc3/react-board';
 import StyleGuide_board_module from './style-guide.board.module.scss';
+import Classnames from 'classnames';
 
 export default createBoard({
     name: 'StyleGuide',
-    Board: () => <div className={StyleGuide_board_module.root}></div>,
+    Board: () => <div className={Classnames(StyleGuide_board_module.root)}></div>,
     isSnippet: true,
 });
