bf96e3bc514f9f30cdc5455926c999e30eed81ff
Codux <codux@codux.com>
2024-03-27 15:27:49 +0200

    Added a sections

--- src/_codux/boards/style-guide.board.tsx
+++ src/_codux/boards/style-guide.board.tsx
@@ -5,6 +5,28 @@ import Classnames from 'classnames';
 
 export default createBoard({
     name: 'StyleGuide',
-    Board: () => <div className={Classnames(StyleGuide_board_module.root)}></div>,
+    Board: () => (
+        <div className={Classnames(StyleGuide_board_module.root)}>
+            <div className={StyleGuide_board_module.pageHeader}>
+                <h1 className={StyleGuide_board_module.TitleHeader}>Style Guide</h1>
+                <span className={StyleGuide_board_module.logo}>LOGO</span>
+            </div>
+            <div className={StyleGuide_board_module.SectionContainer}>
+                <div className={StyleGuide_board_module.SectionTitle}>
+                    <img src="/src/assets/react.svg" />
+                    <h2>Typography</h2>
+                </div>
+                <p className={StyleGuide_board_module.SectionSubtitle}>
+                    Defines the fonts used for headings, body text, labels, etc., including size,
+                    weight, and spacing.
+                </p>
+            </div>
+        </div>
+    ),
     isSnippet: true,
+    environmentProps: {
+        windowWidth: 1014,
+        canvasWidth: 1021,
+        canvasHeight: 644,
+    },
 });
