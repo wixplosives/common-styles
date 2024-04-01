15b1378ad4021f64c6f65e78638cee7f437d4e80
Codux <codux@codux.com>
2024-04-01 15:04:52 +0300

    remove color section

--- src/_codux/boards/style-guide.board.tsx
+++ src/_codux/boards/style-guide.board.tsx
@@ -149,52 +149,6 @@ export default createBoard({
                     </div>
                 </div>
             </div>
-            <div className={StyleGuide_board_module.SectionContainer}>
-                <div className={StyleGuide_board_module.SectionTitle}>
-                    <img src="/src/assets/react.svg" />
-                    <h2>Color</h2>
-                </div>
-                <p className={StyleGuide_board_module.SectionSubtitle}>
-                    Colors are a powerful tool in design for communicating meaning and influencing
-                    user perception. Here&apos;s colors can contribute to the success of your design
-                    by conveying specific
-                </p>
-            </div>
-            <div className={StyleGuide_board_module.subsectionContainer}>
-                <h2 className={StyleGuide_board_module.subSectionTitle}>Primary / Blue</h2>
-                <div className={StyleGuide_board_module.grid1}>
-                    <div className={StyleGuide_board_module.coloContainer}>
-                        <div className={Classnames(StyleGuide_board_module.subColor, 'b10')}></div>
-                        <span className={StyleGuide_board_module.subtitleValue}>B10 </span>
-                        <span className={StyleGuide_board_module.subtitleValue}>#3153AF</span>
-                    </div>
-                    <div className={StyleGuide_board_module.coloContainer}>
-                        <div className={Classnames(StyleGuide_board_module.subColor, 'b20')}></div>
-                        <span className={StyleGuide_board_module.subtitleValue}>B10 </span>
-                        <span className={StyleGuide_board_module.subtitleValue}>#3153AF</span>
-                    </div>
-                    <div className={StyleGuide_board_module.coloContainer}>
-                        <div className={Classnames(StyleGuide_board_module.subColor, 'b30')}></div>
-                        <span className={StyleGuide_board_module.subtitleValue}>B10 </span>
-                        <span className={StyleGuide_board_module.subtitleValue}>#3153AF</span>
-                    </div>
-                    <div className={StyleGuide_board_module.coloContainer}>
-                        <div className={Classnames(StyleGuide_board_module.subColor, 'b40')}></div>
-                        <span className={StyleGuide_board_module.subtitleValue}>B10 </span>
-                        <span className={StyleGuide_board_module.subtitleValue}>#3153AF</span>
-                    </div>
-                    <div className={StyleGuide_board_module.coloContainer}>
-                        <div className={Classnames(StyleGuide_board_module.subColor, 'b10')}></div>
-                        <span className={StyleGuide_board_module.subtitleValue}>B10 </span>
-                        <span className={StyleGuide_board_module.subtitleValue}>#3153AF</span>
-                    </div>
-                    <div className={StyleGuide_board_module.coloContainer}>
-                        <div className={Classnames(StyleGuide_board_module.subColor, 'b10')}></div>
-                        <span className={StyleGuide_board_module.subtitleValue}>B10 </span>
-                        <span className={StyleGuide_board_module.subtitleValue}>#3153AF</span>
-                    </div>
-                </div>
-            </div>
         </div>
     ),
     isSnippet: true,
